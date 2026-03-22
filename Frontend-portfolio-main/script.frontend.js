68162d9ef52eb61e9bfe376089b23acb3a560f27
Nithin <your-email@example.com>
2026-03-22 12:33:44 +0530

    Update script.js to fetch Render backend

--- Frontend-portfolio-main/script.frontend.js
+++
@@ -1,9 +0,0 @@
-script.js
-// script.js
-https://my-backend-9b3i.onrender.com
-  then(response => response.json())
-  .then(data => {
-    console.log(data); // check message in browser console
-    document.getElementById('backend-message').innerText = data.message;
-  })
-  .catch(err => console.error(err));
